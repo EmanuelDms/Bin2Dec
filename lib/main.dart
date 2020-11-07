c18872b8e42dacb6986bd7d9c28304de925e7753
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-11-07 19:48:47 -0300

    feat: add default text theme Text Theme: Righteous

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'Home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Binary to Decimal",
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-  ));
+  runApp(
+    MaterialApp(
+      title: "Binary to Decimal",
+      home: Home(),
+      theme: ThemeData(
+        textTheme: GoogleFonts.righteousTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
